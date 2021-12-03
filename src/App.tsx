b82684ea11dc3ae5a622f8cab7096e1f47805026
aimenin <artem.igorevich8@rambler.ru>
2021-12-03 15:10:46 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoStore from './stores/TodoStore';
+import TodoInput from './Todo/TodoInput';
+import TodoList from './Todo/TodoList';
+
+const todos = new TodoStore();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <TodoInput todos={todos}/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
---
+++ src/Todo/TodoInput/index.tsx
@@ -0,0 +1,23 @@
+import { FormEvent } from "react";
+import TodoStore from "../../stores/TodoStore";
+import styles from "./TodoInput.module.css";
+
+const TodoInput = ({todos}: {todos: TodoStore}) => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const formElement = e.target as HTMLFormElement;
+    const formData = new FormData(formElement);
+
+    const value = String(formData.get("todo-input") || "");
+    todos.add(value);
+    formElement.reset();
+  }
+
+  return <form onSubmit={handleSubmit} className={styles["todo-input-group"]}>
+    <input name="todo-input" placeholder="Add todo ..."/>
+    <button type="submit">Add Todo</button>
+  </form>
+}
+
+export default TodoInput;
