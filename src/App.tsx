c49a57286c01f14ee852a3e31cc43fb9946de0c4
aimenin <artem.igorevich8@rambler.ru>
2021-12-06 17:47:15 +0300

    add simple useState in todosList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoInput from './Todo/TodoInput';
 import TodoList from './Todo/TodoList';
+import styles from "./App.module.css";
 
 function App() {
+  const [todosVisible, setTodosVisible] = useState(true);
+
+  const handleVisible = () => setTodosVisible(state => !state);
+
   return (
     <div className="app">
       <TodoInput />
-      <TodoList />
+      <div className={styles['todo-list-wrapper']}>
+        <h2 onClick={handleVisible}>
+          <span>{todosVisible ? "-" : "+"}</span>
+          Todos
+        </h2>
+        {todosVisible && <TodoList />}
+      </div>
     </div>
   );
 }
