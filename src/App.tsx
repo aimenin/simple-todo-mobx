1debf9fe87625d55827e9ba3f757c8c3c3eef8dc
aimenin <artem.igorevich8@rambler.ru>
2021-12-06 17:53:27 +0300

    change useState function on observable base function

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,31 @@ import React, { useState } from 'react';
 import TodoInput from './Todo/TodoInput';
 import TodoList from './Todo/TodoList';
 import styles from "./App.module.css";
+import { observable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [todosVisible, setTodosVisible] = useState(true);
+  const [appUI] = useState(() => observable({
+    todosVisible: true,
+    toggleTodoVisibility() {
+      this.todosVisible = !this.todosVisible;
+    },
+  }));
 
-  const handleVisible = () => setTodosVisible(state => !state);
+  const handleVisible = () => appUI.toggleTodoVisibility();
 
   return (
     <div className="app">
       <TodoInput />
       <div className={styles['todo-list-wrapper']}>
         <h2 onClick={handleVisible}>
-          <span>{todosVisible ? "-" : "+"}</span>
+          <span>{appUI.todosVisible ? "-" : "+"}</span>
           Todos
         </h2>
-        {todosVisible && <TodoList />}
+        {appUI.todosVisible && <TodoList />}
       </div>
     </div>
   );
 }
 
-export default App;
+export default observer(App);
