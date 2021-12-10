1a4f2bc8de5b8a2a03d7672e5d98803c8c59ca3c
aimenin <artem.igorevich8@rambler.ru>
2021-12-10 18:50:37 +0300

    add example of reaction autorun

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoInput from './Todo/TodoInput';
 import TodoList from './Todo/TodoList';
 import styles from "./App.module.css";
 import { observer, useLocalObservable } from 'mobx-react-lite';
 import { useStore } from './stores';
+import { autorun } from 'mobx';
 
 function App() {
   const {todos} = useStore();
+
+  useEffect(() => {
+    const disposeAutorun = autorun(() => {
+      console.log(todos.list.length);
+      throw new Error('new error'); // все ошибки будут выводится только в консоль и не будут прерывать исполнение нашего приложения
+    }, {
+      delay: 1000,
+      onError: err => console.log(err.message), // обработка ошибок
+    }) // любые действия, которые мы сделаем в течении секунды будут отменены
+
+    return () =>  {
+      disposeAutorun(); // делаем такую конструкцию, чтобы избежать утечки памяти
+    }
+  }, [])
 
   const appUI = useLocalObservable(() => ({
     todosVisible: true,
