c43bfb4ca4706112a490b6ab17d3dac340f9c287
aimenin <artem.igorevich8@rambler.ru>
2021-12-10 19:16:46 +0300

    add example of reaction when

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,18 @@ import TodoList from './Todo/TodoList';
 import styles from "./App.module.css";
 import { observer, useLocalObservable } from 'mobx-react-lite';
 import { useStore } from './stores';
-import { reaction } from 'mobx';
+import { when } from 'mobx';
 
 function App() {
   const {todos} = useStore();
 
   useEffect(() => {
-    const disposeReaction = reaction(
-    () => {
-      return {length: todos.list.length, unfinishedTodos: todos.unfinishedTodos};
-    },
-    (oldValue, newValue) => {
-      console.log({ newValue, oldValue }); // oldValue и newValue - это реальный useCase для использования reactions
-    }, {
-      delay: 1000,
-      onError: err => console.log(err.message), // обработка ошибок
-    }) // любые действия, которые мы сделаем в течении секунды будут отменены
-
-    return () =>  {
-      disposeReaction(); // делаем такую конструкцию, чтобы избежать утечки памяти
-    }
+    when(
+      () => !appUI.todosVisible, // условие
+      () => {
+        console.log("clean up!");
+      } // реакция
+    ) 
   }, [])
 
   const appUI = useLocalObservable(() => ({
